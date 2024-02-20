801a2e5c94bbd97e617db68eff33eb24595b6805
Cruella7 <118308022+Cruella7@users.noreply.github.com>
2024-02-20 14:46:54 +0300

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,38 @@
+class Player:                                       #Игрок
+    def __init__(self, score,password,login,record):
+        self.score = score
+        self.password = image
+        self.login = login
+        self.record = record
+        #To Do
+        
+class Cosm_ship:                                    #Космический корабль
+    def __init__(self, lenght,color,login):
+        self.weight = weight
+        self.lenght = lenght
+        self.color = color
+        #To Do
+        
+class Bullet:                                       #Пуля
+    def __init__(self, lenght,color,login,speed):
+        self.weight = weight
+        self.lenght = lenght
+        self.color = color
+        seLf.speed = speed
+        #To Do        
+    
+class Explosion:                                    #Взрыв
+    def __init__(self, lenght,color,login,speed):
+        self.weight = weight
+        self.lenght = lenght
+        self.color = "Оранжевый"
+        #To Do   
+        
+class Stone:                                        #Препятствие - камень
+    def __init__(self, lenght,color,login,speed):
+        self.weight = weight
+        self.lenght = lenght
+        self.color = color
+        #To Do         
+
+        
